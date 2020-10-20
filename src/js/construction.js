99eb9ea0d72a8ce89072e6a42b9228f8844301d5
João Victor <joaovictor-san-co@hotmail.com>
2020-10-19 22:22:15 -0300

    update

--- src/js/construction.js
+++ src/js/construction.js
@@ -1,5 +1,8 @@
 const el = document.getElementsByClassName('paragraph')[0];
+const height = getComputedStyle(el).height;
+console.log(height)
 el.setAttribute('class', 'paragraph-start');
 setTimeout(() => {
-  el.setAttribute('id', 'paragraph-show')
-}, 1000);
+  el.setAttribute('class', 'paragraph-show');
+  el.style.height = '610px';
+}, 1200);
